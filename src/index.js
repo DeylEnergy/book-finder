d32ed02d3201370fbebb895d7c91083e4dccb2b3
Deyl <soulvisman@gmail.com>
2019-03-01 05:27:42 +0600

    Setting up Base && Search components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Search extends React.Component {
-
+  render() {
+    return (
+      <div id="search-block">
+        <input type="text" />
+        <button id="search-btn">Search</button>
+      </div>
+    );
+  }
 }
 
 class Book extends React.Component {
@@ -15,5 +22,19 @@ class Books extends React.Component {
 }
 
 class Base extends React.Component {
+  render() {
+    return (
+      <div id="wrapper">
+        <header>
+          <h1>Book Finder</h1>
+        </header>
+        <div id="content">
+          <Search />
+        </div>
+      </div>
+    );
+  }
 
 }
+
+ReactDOM.render(<Base />, document.getElementById('root'));
