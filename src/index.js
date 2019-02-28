d44b8e1302ad39eba92f629134f282e7a1e5c1c2
Deyl <soulvisman@gmail.com>
2019-02-28 06:36:55 +0600

    Init base of index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Search extends React.Component {
+
+}
+
+class Book extends React.Component {
+
+}
+
+class Books extends React.Component {
+
+}
+
+class Base extends React.Component {
+
+}
