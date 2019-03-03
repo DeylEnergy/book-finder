db785aef80e7e31127313afa72113e9c5cf6e343
Deyl <soulvisman@gmail.com>
2019-03-03 13:40:58 +0600

    Enhanced book's block

--- src/index.js
+++ src/index.js
@@ -37,18 +37,18 @@ class Book extends React.Component {
         </div>
   
         <div className="book__desc">
-          <div className="book__info">
-            <span className="book__author" title="author">{author || 'Not Labeled'}</span>
-            &nbsp;
-            <span className="book__publisher">
-              {publisher}
-            </span>
-            &nbsp;
-            <span className="book__published">{published || 'n/a'}</span>
+          <div className="book__field" title="author">
+            <strong>Author:</strong> {author || 'Not Labeled'}
           </div>
-          <p>
-            {desc} 
-          </p>
+          <div className="book__field">
+            <strong>Publisher:</strong> {publisher}
+          </div>
+          <div className="book__field">
+            <strong>Published:</strong> {published || 'n/a'}
+          </div>
+          <div className="book__field">
+          {desc} 
+          </div>
         </div>
       </div>
     );
